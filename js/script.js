7ef2e094bc5a437963831fdd642617cc0043e9e2
amitbhudiygit config --global user.name amitbhudiyaa <bhudiyaamit10@gmail.com>
2025-03-25 16:21:47 +0800

    Refactor CSS styles for improved layout and design; add JavaScript for progress bar functionality

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+
+
+
+// Select the element you want to apply the background size to
+const element = document.querySelector('.progress-1'); // Replace 'your-selector-here' with the actual selector
+
+// Set the background size to 50% 20%
+element.style.backgroundSize = '50%';
