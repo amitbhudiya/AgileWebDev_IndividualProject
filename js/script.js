2b30fa17103e4e88e5bb4da24c5ebdc1467c7db2
amitbhudiygit config --global user.name amitbhudiyaa <bhudiyaamit10@gmail.com>
2025-03-31 16:30:19 +0800

    Enhance progress tracking functionality

--- js/script.js
+++ js/script.js
@@ -1,22 +1,11 @@
 
 
-
-
-
-
-
-
-
-
-
 let totalhtmlselected = 0;
 let totalcssselected = 0;
 let totaljavascriptselected = 0;
 let totalhtml = 0;
 let totalcss = 0;
 let totaljs = 0;
-
-
 
 function selected() {
     let htmlinput = document.getElementsByName('html-checkbox');
@@ -42,6 +31,27 @@ function selected() {
     let total = totalhtml + totalcss + totaljs;
     let selected = totalhtmlselected + totalcssselected + totaljavascriptselected;
     let progress = (selected / total) * 100;
+    if (progress >= 50) {
+        document.querySelector(".progress-bar").style.color = "#ffffff";
+    }
+    else {
+        document.querySelector(".progress-bar").style.color = "#000000";
+    }
+    if (selected > 0) {
+        document.querySelector(".track").innerHTML = "You've implemented " + selected + " out of " + total + " best practices.";
+    }
+    else {
+        document.querySelector(".track").innerHTML = "Track your progress toward implementing web development best practices.";
+    }
+    if (selected >= (total - 2)) {
+        document.querySelector(".success-criteria").innerHTML = "Excellent! Your code follows industry-standard best practices.";
+        catimageajax()
+    }
+    else {
+        document.querySelector(".success-criteria").innerHTML = "Implement at least " + (total - 2) + " practices to meet the successful criteria.";
+    }
+
+    document.querySelector(".progress-bar").style.backgroundSize = progress + "%";
     document.querySelector(".progress-bar").style.backgroundSize = progress + "%";
     document.querySelector(".progress-bar").innerHTML = progress + "%";
     totalhtmlselected = 0;
@@ -50,5 +60,10 @@ function selected() {
     totalcss = 0;
     totaljavascriptselected = 0;
     totaljs = 0;
+
+
+}
+function catimageajax() {
+    document.querySelector(".summary-bottom").style.display = "block";
 }
 selected();
